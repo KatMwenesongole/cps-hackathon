732515c829cf5d91958fa78143aa537b5a8f9d9c
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2025-11-05 12:26:44 +0100

    new fucntion

--- utils.py
+++ utils.py
@@ -85,3 +85,25 @@ def get_latest_temperature_data(device: Literal['device01', 'device02', 'device0
             print(f"Latest value: {latest_value}")
 
     return latest_value
+
+def get_temperature_data_in_range(device: Literal['device01', 'device02', 'device03', 'device04'], start: str, end: str) -> pd.DataFrame:
+    token = os.environ.get("INFLUXDB_TOKEN") or PREDICTIVE_MAIN_INFLUXDB_TOKEN
+    org = "cnidome"
+    url = "https://influx.cps-predictive-maintenance.skylyze.com"
+
+    client = InfluxDBClient(url=url, token=token, org=org)
+    query_api = client.query_api()
+
+    query_str = f"""
+    from(bucket: "home")
+    |> range(start: {start}, stop: {end})
+    |> filter(fn: (r) => r["_measurement"] == "temperature")
+    |> filter(fn: (r) => r["_field"] == "temperature")
+    |> filter(fn: (r) => r["device"] == "{device}")
+    |> aggregateWindow(every: 5s, fn: mean, createEmpty: true)
+    |> yield(name: "mean")
+    """
+
+    df = query_api.query_data_frame(query_str)
+
+    return df
