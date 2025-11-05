05dbbe31345bf69d80dda8416395cae7dfcbde35
Kat Mwenesongole <katmwenesongole@gmail.com>
2025-11-05 12:04:56 +0100

    initial

---
+++ main.py
@@ -0,0 +1,29 @@
+from utils import get_latest_temperature_data
+latest_value = get_latest_temperature_data('device02')
+print(latest_value)
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets, QtCore
+import numpy as np
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+win = pg.GraphicsLayoutWidget(show=True, title="Live Graph Example")
+
+plot = win.addPlot(title="Sine Wave")
+curve = plot.plot(pen='y')
+
+data = np.linspace(0, 2*np.pi, 1000)
+phase = 0
+
+def update():
+    global phase
+    y = np.sin(data + phase)
+    curve.setData(y)
+    phase += 0.1
+
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)  # update every 50 ms (20 Hz)
+
+sys.exit(app.exec_())
